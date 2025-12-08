e2f0b2175596dcbda35a253224f7b0e23b2f8733
Aishwarya Kundur <akundur17@gmail.com>
2025-12-07 18:05:11 -0600

    users CRUD

---
+++ usersServer.js
@@ -0,0 +1,90 @@
+
+const express = require('express');
+const app = express();
+app.use(express.json());
+
+let users = [];
+
+//Get All Users
+app.get('/users', (req, res) => {
+    res.status(200).json(users);
+})
+
+app.get('/users/:id', (req, res) => {
+    const user = users.find(u => u.id === Number(req.params.id));
+
+    if(!user) return res.status(404).json({
+        message: "User Not Found"
+    })
+
+    res.json(user);
+})
+
+
+//Add User
+app.post('/users', (req, res) => {
+    const {id, name, age} = req.body;
+
+    const newUser = {
+        id: users.length + 1,
+        name,
+        age
+    }
+
+    users.push(newUser);
+
+    res.status(201).json({
+        message: "User Added",
+        user: newUser
+    })
+})
+
+
+//Update User
+app.put('/users/:id', (req, res) => {
+    const user = users.find(u => u.id === Number(req.params.id));
+
+    if(!user) return res.status(404).json({
+        message: "User not found"
+    })
+
+    const {name, age} = req.body;
+
+    user.name = name ||  user.name;
+    user.age = age ||  user.age;
+
+    res.status(200).json({
+        message: "user updated",
+        user: user
+    })
+
+})
+
+
+//Delete User
+app.delete('/users/:id', (req, res) => {
+
+    const index = users.findIndex(u => u.id === Number(req.params.id));
+
+    if(index === -1) return res.status(404).json({
+        message: " user not found"
+    })
+
+    users.splice(index, 1);
+
+    res.status(200).json({
+        message: "User Deleted"
+    })
+    
+})
+
+
+//Global Error Handler
+app.use((err, req, res, next) => {
+    console.log(err.stack);
+    res.status(500).json({
+        message: "Internal Server Error",
+        error: err.message
+    })
+});
+app.listen(3000, () => console.log("Sever is running on port:3000"))
